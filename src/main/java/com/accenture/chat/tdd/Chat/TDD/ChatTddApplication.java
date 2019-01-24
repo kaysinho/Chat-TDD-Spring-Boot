5ef51db35ee88f001869baf387046e1c4960076e
Jhonatan Plata <jplata@bancolombia.com.co>
2019-01-24 12:10:37 -0500

    Primer commit

---
+++ src/main/java/com/accenture/chat/tdd/Chat/TDD/ChatTddApplication.java
@@ -0,0 +1,14 @@
+package com.accenture.chat.tdd.Chat.TDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTddApplication.class, args);
+	}
+
+}
+
